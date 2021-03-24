dfa32239ee13d2c96b3e1cf123c36bd206b6ecea
Ann02 <annatvorydlo@gmail.com>
2021-03-24 23:42:37 +0000

    Sample code

---
+++ enter.js
@@ -0,0 +1,6 @@
+let login = prompt("Enter login", "user");
+let email = prompt("Enter email", "usermale@gmail.com");
+let password = prompt("Enter password", "qwerty");
+alert ('Dear ' + login);
+alert ('Your email is ' + email);
+alert ('Your password is ' + password);
